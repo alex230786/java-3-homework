68e7065deb2a10c39613c3fd31bd2cb8aea946e0
Aleksey Varlamov <avarlamov3@mail.ru>
2023-05-27 19:03:52 +0300

    src2

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int replenishment_amount = 1100;
-        int amout_on_account = 100;
+        int replenishmentAmount = 1100;
+        int amoutOnAccount = 100;
         int bonus;
-        if (replenishment_amount > 1000) {
-            bonus = replenishment_amount / 100;
+        if (replenishmentAmount > 1000) {
+            bonus = replenishmentAmount / 100;
                             } else {
             bonus = 0;
         }
-        int total = amout_on_account + replenishment_amount + bonus;
+        int total = amoutOnAccount + replenishmentAmount + bonus;
 
         System.out.println("Сумма на счету: " +total);
     }
